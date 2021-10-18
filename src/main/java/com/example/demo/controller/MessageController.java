b329b127bb79de480f69249a32e89eef072b152b
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-10-18 22:19:16 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+	
+	@GetMapping("/message")
+	//@ResponseBody
+	public String message(Model model) {
+		model.addAttribute("name", "<h1>John</h1>");
+		List<Integer> scores = Arrays.asList(100, 40, 70, 55); 
+		model.addAttribute("scores", scores);
+		return "message";
+	}
+	
+}
